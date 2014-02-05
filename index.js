8a163729a0210e83ea5c25cb7bb2163ad25b7ec1
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2014-02-04 22:01:36 -0500

    Restructuring the lib files

---
+++ index.js
@@ -0,0 +1,13 @@
+var chord = require("./lib/arcus-chord"),
+    convert = require("./lib/arcus-convert"),
+    wheel = require("./lib/arcus-wheel");
+
+module.exports = {
+
+    Chord: chord,
+    
+    converter: convert,
+    
+    wheel: wheel
+
+};
